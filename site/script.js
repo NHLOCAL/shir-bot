f61f4c3f0708708617dcaabc61783fcb2e0a4ae9
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2023-07-09 00:20:41 +0300

    שינוי ניסוח

--- site/script.js
+++ site/script.js
@@ -104,7 +104,7 @@ function displayResults(results) {
       if (!song.album.toLowerCase().includes('סינגלים')) {
         serialLink.addEventListener('click', function(event) {
           event.preventDefault();
-          alert('כרגע רק סינגלים נשלחים, נא נסה שיר אחר!');
+          alert('באתר זה נשלחים סינגלים בלבד, נא נסה שיר אחר!');
         });
       } else {
         serialLink.href = generateMailtoLink(song.serial);

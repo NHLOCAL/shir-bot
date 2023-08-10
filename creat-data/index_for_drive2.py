6e3d3aedb2465bd59625963a4d3ec390a0dc5531
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2023-08-10 06:07:00 +0300

    Update index_for_drive2.py

--- creat-data/index_for_drive2.py
+++ creat-data/index_for_drive2.py
@@ -1,6 +1,5 @@
 import csv
 import os
-import json
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
@@ -16,69 +15,35 @@ def main(FOLDER_ID, singer_name):
     service = build('drive', 'v3', credentials=credentials)
 
     # Create a CSV file and write headers
-    with open('output.csv', 'a', newline='') as csvfile:
+    csv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output.csv')
+    is_new_file = not os.path.exists(csv_path)
+    
+    with open(csv_path, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
+        if is_new_file:
+            writer.writerow(["Serial Number", "Filename", "Album", "Singer", "File ID"])
 
         # Initialize serial number counter
         serial_number = 1
 
         # Traverse folder and process files
-        process_folder(service, writer, FOLDER_ID, serial_number)
+        process_folder(service, writer, FOLDER_ID, serial_number, singer_name)
 
     print('CSV file created successfully.')
 
-def process_folder(service, writer, folder_id, serial_number, parent_folder_id=None):
-    # Get all files in the current folder
-    files = service.files().list(
-        q=f"'{folder_id}' in parents and mimeType != 'application/vnd.google-apps.folder'",
-        fields="files(id, name, parents)"
-    ).execute()
+def process_folder(service, writer, folder_id, serial_number, singer_name, parent_folder_id=None):
+    # Rest of your process_folder function
 
-    # Process files in the current folder
-    for file in files['files']:
-        filename = file['name']
-        album = get_folder_name(service, file['parents'][0])
-        singer = singer_name
-        file_id = file['id']
-
-        writer.writerow([serial_number, filename, album, singer, file_id])
-        serial_number += 1
-
-    # Traverse subfolders recursively
-    subfolders = service.files().list(
-        q=f"'{folder_id}' in parents and mimeType = 'application/vnd.google-apps.folder'",
-        fields="files(id)"
-    ).execute()
-
-    for subfolder in subfolders['files']:
-        serial_number = process_folder(service, writer, subfolder['id'], serial_number, folder_id)
-
-    return serial_number
-
-def get_folder_name(service, folder_id, is_singer=False):
-    # Retrieve folder metadata
-    folder = service.files().get(fileId=folder_id, fields='name, parents').execute()
-    folder_name = folder['name']
-
-    if is_singer:
-        # Check if the folder has a parent
-        if 'parents' in folder:
-            parent_folder_id = folder['parents'][0]
-            return get_folder_name(service, parent_folder_id, True)
-        else:
-            return folder_name
-    else:
-        return folder_name
-
+# Rest of your get_folder_name function
 
 def run_now():
-
-    csv_path = 'home/runner/work/shir-bot/shir-bot/creat-data/list-singers.csv'
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(script_dir, 'list-singers.csv')
     
     with open(csv_path, 'r') as file:
         csv_reader = csv.reader(file)
         singer_list = [tuple(row) for row in csv_reader]
-    global singer_name
+        
     for singer_name, FOLDER_ID in singer_list:
         try:
            main(FOLDER_ID, singer_name)
@@ -86,8 +51,7 @@ def run_now():
             try:
                 main(FOLDER_ID, singer_name)
             except:
-                print('was error')
-
+                print('There was an error processing:', singer_name)
 
 if __name__ == '__main__':
     run_now()

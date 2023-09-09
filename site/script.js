ca8ea2b540cf8539c895631561f730ee0dafad04
NHLOCAL <nh.local11@gmail.com>
2023-09-10 01:31:42 +0300

    שדרוג הודעת שגיאה

--- site/script.js
+++ site/script.js
@@ -436,19 +436,33 @@ function openShareModal(shareLink) {
 
 // Function to show a message in the center modal overlay
 function showMessage(message) {
-  var modalOverlay = document.getElementById('modalOverlay');
-  var modalMessage = document.getElementById('modalMessage');
-  var modalOkButton = document.getElementById('modalOkButton');
+var modalOverlay = document.getElementById('modalOverlay');
+var modalMessage = document.getElementById('modalMessage');
+var modalOkButton = document.getElementById('modalOkButton');
 
-  modalMessage.textContent = message;
-  modalOverlay.style.display = 'flex'; // Display the modal overlay
+modalMessage.textContent = message;
+modalOverlay.style.display = 'flex'; // Display the modal overlay
 
-  // Add event listener to OK button
-  modalOkButton.addEventListener('click', hideMessage);
+// Add event listener to OK button
+modalOkButton.addEventListener('click', hideMessage);
+
+// Add event listener to document to close on click anywhere
+document.addEventListener('click', function(event) {
+  if (event.target === modalOverlay) {
+	hideMessage();
+  }
+});
+
+// Add event listener to document to close on Enter key press
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+	hideMessage();
+  }
+});
 }
 
 // Function to hide the modal overlay
 function hideMessage() {
-  var modalOverlay = document.getElementById('modalOverlay');
-  modalOverlay.style.display = 'none'; // Hide the modal overlay
+var modalOverlay = document.getElementById('modalOverlay');
+modalOverlay.style.display = 'none'; // Hide the modal overlay
 }

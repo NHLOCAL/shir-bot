f0adc8cc2799fa12a1ba601cbe6fb331faa31254
NHLOCAL <nh.local11@gmail.com>
2023-10-02 20:12:28 +0300

    Update new-web.js

--- site/new-web.js
+++ site/new-web.js
@@ -92,8 +92,8 @@ updateFooterContent();
 // רישום אירוע עבור ניתוח גוגל
 function conversion_music_drive() {
   // Send the event to Google Analytics
-  gtag('event', 'conversion', {
-    'event_category': 'Registration',
+  gtag('event', 'Registration', {
+    'event_category': 'music_drive',
 	'value': 1, // NIS 1
 	'currency': 'NIS' // Currency type
   });

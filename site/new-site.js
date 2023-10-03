94f1c0584cefaea31b9f6679c6ab28b671bd694c
NHLOCAL <nh.local11@gmail.com>
2023-10-03 17:20:54 +0300

    שינוי שם

--- site/new-site.js
+++ site/new-site.js
@@ -89,12 +89,3 @@ setInterval(updateFooterContent, 10000);
 // Step 6: Call the updateFooterContent function initially to display the first content.
 updateFooterContent();
 
-// רישום אירוע עבור ניתוח גוגל
-function conversion_music_drive() {
-  // Send the event to Google Analytics
-  gtag('event', 'Registration', {
-    'event_category': 'music_drive',
-	'value': 1, // NIS 1
-	'currency': 'NIS' // Currency type
-  });
-};

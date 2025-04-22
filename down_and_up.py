60d11e9de0e13be99a38c41b2c726243e5ab1ce6
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-04-22 22:05:35 +0300

    Update down_and_up.py

--- down_and_up.py
+++ down_and_up.py
@@ -117,7 +117,7 @@ def download_mp3(media_url, desired_filename=None):
     temp_download_path = SCRIPT_DIR / local_filename
     logging.info(f"Downloading: {media_url} to {temp_download_path}")
 
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0'} # בדוק גרסאות עדכניות אם חשוב
 
     try:
         with requests.get(media_url, headers=headers, stream=True, timeout=60) as r: # Added timeout
